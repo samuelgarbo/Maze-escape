8bd37a527c8fbafe65b8087af55faf2f4aefbde2
samuelgarbo <samuel.garbo@gmail.com>
2016-11-05 15:18:55 +0900

    Create main.c
    나중에 기능 추가

---
+++ main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+//Maze node
+typedef struct node {
+	int value;
+	struct node *right;
+	struct node *left;
+	struct node *up;
+	struct node *down;
+}node;
+
+int main(){
+
+return 0;
+
+}
